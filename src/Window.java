f1b80f0707d26778c1d238d911abb11c3c391b05
Julian <julianherbert.2007@gmail.com>
2025-05-05 12:48:53 -0400

    update window

--- src/Window.java
+++ src/Window.java
@@ -7,11 +7,26 @@ class Window extends JFrame {
         setTitle("Rosiepad");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(800, 600);
+        setLaf("FlatLightLaf");
 
         setVisible(true);
     }
 
     public void setLaf(String lafName) {
-        
+        try {
+            if (lafName.startsWith("Flat")) {
+                UIManager.setLookAndFeel("com.formdev.flatlaf." + lafName);
+            } else {
+                for (UIManager.LookAndFeelInfo lafInfo : UIManager.getInstalledLookAndFeels()) {
+                    if (lafInfo.getName().equals(lafName)) {
+                        UIManager.setLookAndFeel(lafInfo.getClassName());
+                        break;
+                    }
+                }
+            }
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
